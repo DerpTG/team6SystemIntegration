4335212c250c4ac455378a4a42c81702ef95f6b5
Eyan Jaffery <124312130+eyanjaffery@users.noreply.github.com>
2024-04-20 20:06:51 -0400

    RabbitMQ Sender and Receiver setup.

--- src/main/java/org/example/RabbitMQReceiver.java
+++ src/main/java/org/example/RabbitMQReceiver.java
@@ -1,3 +1,12 @@
+/** Project: Group 6 Team Project
+ * Purpose Details: System Integration Using RabbitMQ, JSON, and CaeserCipher Encryption
+ * Course: IST 242
+ * Author: Felix Naroditskiy, Eyan Jaffery, Lasha Kaliashvili, Michael Litka, Houde Yu
+ * Date Developed: 4/19/2024
+ * Last Date Changed: 4/20/2024
+ * Rev: 1.0
+ */
+
 package org.example;
 
 public class RabbitMQReceiver {
