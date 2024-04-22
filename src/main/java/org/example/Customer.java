167397ce9d26c534c35cc2a55f6942cea45b6229
DerpTG <fnaroditskiy@gmail.com>
2024-04-22 18:59:23 -0400

    headers

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -3,7 +3,7 @@
  * Course: IST 242
  * Author: Felix Naroditskiy, Eyan Jaffery, Lasha Kaliashvili, Michael Litka, Houde Yu
  * Date Developed: 4/19/2024
- * Last Date Changed: 4/20/2024
+ * Last Date Changed: 4/22/2024
  * Rev: 1.0
  */
 
